043ca2d85c9529af5d255675784c7b473bc841be
dzhelezov <dzhelezov@gmail.com>
2023-02-08 15:27:12 +0100

    use archive registry

--- src/processor.ts
+++ src/processor.ts
@@ -17,7 +17,7 @@ export const FACTORY_ADDRESS = '0x1f98431c8ad98523631ae4a59f267346ea31f984'
 
 let processor = new EvmBatchProcessor()
     .setDataSource({
-        archive: 'https://eth.archive.subsquid.io',
+        archive: lookupArchive('eth-mainnet'),
     })
     .setBlockRange({
         from: 12_369_621,
