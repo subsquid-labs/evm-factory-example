2db31a8c1425261ce00bb56772b42a457bd6d0f6
dzhelezov <dzhelezov@gmail.com>
2023-02-21 18:24:53 +0100

    fix lookupArchive import

--- src/processor.ts
+++ src/processor.ts
@@ -7,11 +7,13 @@ import {
 } from '@subsquid/evm-processor'
 import {LogItem} from '@subsquid/evm-processor/lib/interfaces/dataSelection'
 import {Store, TypeormDatabase} from '@subsquid/typeorm-store'
+import { lookupArchive } from '@subsquid/archive-registry'
 import {In} from 'typeorm'
 import {Pool, Swap} from './model'
 
 import * as factoryAbi from './abi/factory'
 import * as poolAbi from './abi/pool'
+
 
 export const FACTORY_ADDRESS = '0x1f98431c8ad98523631ae4a59f267346ea31f984'
 
