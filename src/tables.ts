22892d7aaa0536c019e709d681f217a50681e9bd
abernatskiy <abernatskiy@toha.li>
2023-03-18 00:57:30 +0900

    Adding compression to the tables
    	modified:   src/tables.ts

--- src/tables.ts
+++ src/tables.ts
@@ -4,21 +4,33 @@ import {
     Types
 } from '@subsquid/file-store-parquet'
 
-export const Pools = new Table('pools.parquet', {
-    address: Column(Types.String()),
-    token0: Column(Types.String()),
-    token1: Column(Types.String())
-})
+export const Pools = new Table(
+    'pools.parquet',
+    {
+        address: Column(Types.String()),
+        token0: Column(Types.String()),
+        token1: Column(Types.String())
+    },
+    {
+	    compression: 'GZIP'
+    }
+)
 
-export const Swaps = new Table('swaps.parquet', {
-    txHash: Column(Types.String()),
-    blockNumber: Column(Types.Uint64()),
-    timestamp: Column(Types.Timestamp()),
-    pool: Column(Types.String()),
-    sender: Column(Types.String()),
-    amount0In: Column(Types.Decimal(42)),
-    amount1In: Column(Types.Decimal(42)),
-    amount0Out: Column(Types.Decimal(42)),
-    amount1Out: Column(Types.Decimal(42)),
-    to: Column(Types.String())
-})
+export const Swaps = new Table(
+    'swaps.parquet',
+    {
+        txHash: Column(Types.String()),
+        blockNumber: Column(Types.Uint64()),
+        timestamp: Column(Types.Timestamp()),
+        pool: Column(Types.String()),
+        sender: Column(Types.String()),
+        amount0In: Column(Types.Decimal(42)),
+        amount1In: Column(Types.Decimal(42)),
+        amount0Out: Column(Types.Decimal(42)),
+        amount1Out: Column(Types.Decimal(42)),
+        to: Column(Types.String())
+    },
+    {
+	    compression: 'GZIP'
+    }
+)
