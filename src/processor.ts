5b43e56ab444974c928416055c697d87556cb287
abernatskiy <abernatskiy@toha.li>
2023-03-18 17:33:55 +0900

    Minimal squid for reproducing the binance archive performance issue
    	modified:   .env
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/processor.ts
    	deleted:    src/tables.ts

--- src/processor.ts
+++ src/processor.ts
@@ -1,31 +1,14 @@
-import {
-    BatchHandlerContext,
-    BatchProcessorItem,
-    EvmBatchProcessor,
-    EvmBlock,
-} from '@subsquid/evm-processor'
-import {LogItem} from '@subsquid/evm-processor/lib/interfaces/dataSelection'
-import {
-    Store,
-    Database,
-    LocalDest
-} from '@subsquid/file-store'
+import {EvmBatchProcessor} from '@subsquid/evm-processor'
+import {Database, LocalDest} from '@subsquid/file-store'
 import {lookupArchive} from '@subsquid/archive-registry'
-
-// import {Pools, Swaps} from './tables'
 import {Table} from '@subsquid/file-store-json'
-import * as factoryAbi from './abi/factory'
-import * as pairAbi from './abi/pair'
 
-import fs from 'fs'
+import * as factoryAbi from './abi/factory'
 
 const FACTORY_ADDRESSES = new Set([
 	'0xBCfCcbde45cE874adCB698cC183deBcF17952812'.toLowerCase(),
 	'0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'.toLowerCase() // v2
 ])
-
-const outPath = '/mirrorstorage/pancakes-data'
-const outPathStats = '/mirrorstorage/pancakes-stats'
 
 let processor = new EvmBatchProcessor()
     .setDataSource({
@@ -34,7 +17,7 @@ let processor = new EvmBatchProcessor()
     })
     .setBlockRange({
         from: 586_851,
-        to: 26_548_500
+//        to: 26_548_500
     })
     .addLog([...FACTORY_ADDRESSES], {
         filter: [[factoryAbi.events.PairCreated.topic]],
@@ -45,42 +28,16 @@ let processor = new EvmBatchProcessor()
             },
         } as const,
     })
-/*    .addLog([], {
-        filter: [[pairAbi.events.Swap.topic]],
-        data: {
-            evmLog: {
-                topics: true,
-                data: true,
-            },
-            transaction: {
-                hash: true,
-            },
-        } as const,
-    })
-*/
 
-// let tables = { Pools, Swaps }
 let tables = { Pools: new Table<{address: string, factory: string}>('transfers.json') }
 let db = new Database({
     tables: tables,
-    dest: new LocalDest(outPath),
+    dest: new LocalDest('./data'),
     syncIntervalBlocks: 1000,
     chunkSizeMb: 100
 })
 
-type Item = BatchProcessorItem<typeof processor>
-type Ctx = BatchHandlerContext<Store<typeof tables>, Item>
-
-let factoryPools: Set<string>
-let usedContracts = new Map<string, number>()
-let unusedContracts = new Map<string, number>()
-
 processor.run(db, async (ctx) => {
-//    if (!factoryPools) factoryPools = new Set()
-
-//    let poolCreationsData: PoolCreationData[] = []
-//    let swapsData: SwapData[] = []
-
     let poolCreationsData: {address: string, factory: string}[] = []
 
     for (let block of ctx.blocks) {
@@ -89,83 +46,11 @@ processor.run(db, async (ctx) => {
 
             let itemAddr = item.address.toLowerCase()
             if (FACTORY_ADDRESSES.has(itemAddr)) {
-                let pcd = handlePoolCreation(ctx, item)
-                poolCreationsData.push({address: pcd.address, factory: itemAddr})
-//                factoryPools.add(pcd.address)
-//                poolCreationsData.push(pcd)
-            } /* else if (factoryPools.has(itemAddr)) {
-                swapsData.push(handleSwap(ctx, block.header, item))
-                registerItem(itemAddr, usedContracts)
-            } else {
-                registerItem(itemAddr, unusedContracts)
-            }*/
+                let event = factoryAbi.events.PairCreated.decode(item.evmLog)
+                poolCreationsData.push({address: event.pair.toLowerCase(), factory: itemAddr})
+            }
         }
     }
 
-//    keepRecords(usedContracts, `${outPathStats}-used-contracts`)
-//    keepRecords(unusedContracts, `${outPathStats}-unused-contracts`)
-
     ctx.store.Pools.writeMany(poolCreationsData)
-//    ctx.store.Swaps.writeMany(swapsData)
 })
-
-interface PoolCreationData {
-    address: string
-    token0: string
-    token1: string
-}
-
-function handlePoolCreation(ctx: Ctx, item: LogItem<{evmLog: {topics: true; data: true}}>): PoolCreationData {
-    let event = factoryAbi.events.PairCreated.decode(item.evmLog)
-    return {
-        address: event.pair.toLowerCase(),
-        token0: event.token0.toLowerCase(),
-        token1: event.token1.toLowerCase()
-    }
-}
-
-interface SwapData {
-    txHash: string
-    blockNumber: bigint
-    timestamp: Date
-    pool: string
-    sender: string
-    amount0In: bigint
-    amount1In: bigint
-    amount0Out: bigint
-    amount1Out: bigint
-    to: string
-}
-
-function handleSwap(
-    ctx: Ctx,
-    block: EvmBlock,
-    item: LogItem<{evmLog: {topics: true; data: true}; transaction: {hash: true}}>
-): SwapData {
-    let event = pairAbi.events.Swap.decode(item.evmLog)
-    return {
-        txHash: item.transaction.hash,
-        blockNumber: BigInt(block.height),
-        timestamp: new Date(block.timestamp),
-        pool: item.evmLog.address,
-        sender: event.sender.toLowerCase(),
-        amount0In: event.amount0In.toBigInt(),
-        amount1In: event.amount1In.toBigInt(),
-        amount0Out: event.amount0Out.toBigInt(),
-        amount1Out: event.amount1Out.toBigInt(),
-        to: event.to.toLowerCase()
-    }
-}
-
-function registerItem(address: string, records: Map<string, number>) {
-    if(records.has(address)) {
-        records.set(address, records.get(address)!+1)
-    }
-    else {
-        records.set(address, 1)
-    }
-}
-
-function keepRecords(records: Map<string, number>, path: string) {
-    fs.writeFileSync(path, [...records].sort((a, b) => b[1]-a[1]).map(e => `${e[0]} ${e[1]}`).join('\n'))
-}
