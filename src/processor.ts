fb002d21463103f274bcfbab51eabb5759ba14ba
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-06-13 15:08:29 +0500

    bump packages

--- src/processor.ts
+++ src/processor.ts
@@ -18,6 +18,7 @@ export const processor = new EvmBatchProcessor()
         archive: 'https://v2.archive.subsquid.io/network/ethereum-mainnet',
         chain: 'https://rpc.ankr.com/eth',
     })
+    .setFinalityConfirmation(10)
     .setBlockRange({
         from: 12_369_621,
     })
